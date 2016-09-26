e9c3c488fede2b5a607992346207969e13bc3620
Anton Arboleda <anton.arboleda@gmail.com>
2016-09-25 21:32:05 -0700

    created on-click for user profiles

--- js/index.js
+++ js/index.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
             var $tweetsContainer = $('.tweets');
             var tweets = streams.home;
             var index = tweets.length - 1;
+            var activeName = null;
             //console.log(index)
             while (index > 0) {
                 var mostRecent = tweets[index];
-                var $tweet = $('<li id="clickable-tweet" />');
-                $tweet.addClass(mostRecent.user);           
+                var $tweet = $('<li class="clickable-tweet" />');
+                $tweet.attr('name', mostRecent.user);           
                 $tweet.html('@' + mostRecent.user + ': ' + mostRecent.message + ' ' + 'time: ' + parseTime(mostRecent.created_at));
                 $tweetsContainer.append($tweet);
                 index -= 1
@@ -27,40 +28,46 @@ $(document).ready(function() {
                 return hour + ':' + minutes + ampm;
             }
             /*This code is to append new tweets to the timeline*/
+           
             function appendNewTweet() {
                 //get most recent
+
                 var mostRecentTweet = tweets[tweets.length - 1]
 
-                for (var i = 11; i < 12; i++) {
-                    var $newTweet = $('<li id="clickable-tweet" />');
-                    $newTweet.addClass(mostRecentTweet.user);
+                if(activeName === null || activeName === mostRecentTweet.user){
+                    var $newTweet = $('<li class="clickable-tweet" />');
+                    $newTweet.attr('name', mostRecentTweet.user);
                     $newTweet.html('@' + mostRecentTweet.user + ': ' + mostRecentTweet.message + '     time: ' + parseTime(mostRecentTweet.created_at)); //insert the most recent tweet
                     $tweetsContainer.prepend($newTweet)
-                }
+                } 
+                
                 if (i < 50) window.setTimeout(appendNewTweet, Math.random() * 9000)
             }
-            //appendNewTweet();
+            appendNewTweet();
 
 
-            /*This code should append all the tweets of a given user when clickec on */
-            $('body').on('click', '#clickable-tweet', function () {
-      
-              //determine whose tweet it was (by class name)
-              console.log(this.className)
-              var className = this.className; //this refers to the object which was clicked. and its class name
+            /*This code should allow user to see all tweets of a specific person only */
+            $('body').on('click', '.clickable-tweet', function () {
+                console.log($(this).attr('name'))
 
-              //get all the elements by that class name aka all of one persons tweets
-              
-              //remove all the current tweets
-              //append all new tweets to the dom
+                var nameData = $(this).attr('name');
+                activeName = nameData
+                console.log($('li[name='+nameData+']'))
+                var correctStuff = $('li[name='+nameData+']')// a list with all the tweets // get all the tweets within that
+                //to append to the 
+               
+                
+                $('ul').empty();
+                console.log(correctStuff)
+                //..loop thru then  append
+                $.each(correctStuff, function (index, tweet) {
+                    $('ul').append(tweet);
+                })
 
-              
-              //remove current dom
-              //append the elements to the dom
-
-              console.log('testing for clicking list ')
-              //console.log()
-
+                correctStuff.appendTo($('ul'));
+                //this is the element that is clicked on
+                //if it has a class 
+                //find all elements with that class name and append it to the DOM;                
             })
 
             /*This code should submit user input from text area to live feed when tweet buttton is clicked*/
@@ -69,10 +76,12 @@ $(document).ready(function() {
             $button.on("click", function() {
                         //access the tweet from the user
                         var tweet = $("#user-input").val();
+                        $("#user-input").val("");
                         //create a jquery object 
                         var $userTweet = $('<li />')
                         $userTweet.html('user: ' + tweet);
-                        $tweetsContainer.prepend(tweet);
+                        $tweetsContainer.prepend($userTweet);
+
 
 
                     }
